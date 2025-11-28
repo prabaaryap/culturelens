8348a1a9595c44f5bdd8e2dfde10eacfacf85d33
prabaaryap <praba.arya01@gmail.com>
2025-11-29 00:59:12 +0800

    Make DATABASE_URL optional in settings

--- core/config.py
+++ core/config.py
@@ -1,7 +1,12 @@
 from pydantic_settings import BaseSettings
+from typing import Optional
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    # Kita buat ini Optional (boleh kosong) karena di Cloud Run
+    # kita merakit URL-nya secara manual di file database.py
+    DATABASE_URL: Optional[str] = None
+    
+    # Tiga ini TETAP WAJIB karena untuk keamanan Login
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
